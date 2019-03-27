73a22c526aa4db9980b84d77e19350d79435e27b
Герман Игнатович <germanignatovich@MacBook-Pro-German.local>
2019-03-27 22:45:44 +0300

    add gallery

--- src/photoGallery/photoGallert.js
+++ src/photoGallery/photoGallert.js
@@ -9,30 +9,60 @@ const images = [image1, image2, image3, image3, image4, image5, image6];
 
 class Gallery {
     constructor() {
+        this.imageContainer = document.getElementById("image-from-gallery");
         this.galleryContainer = document.getElementById("gallery");
+
         this.backArrow = document.getElementById("arrow-back");
         this.nextArrow = document.getElementById("arrow-next");
-        this.currentImage = 0;
+
+        this.currentIndexImage = 0;
+        this.containerImages = null;
+
+        this.currentImageContainer = document.createElement("img");
+        this.currentImageContainer.className = "current-image";
+        this.imageContainer.appendChild(this.currentImageContainer);
+
+        this.activeImageInGallety = null;
     }
 
     createGallery(images) {
-        const image = document.createElement("img");
 
-        image.src = images[1];
+        this.containerImages = images.map((image, index) => {
+          const imageContainer = document.createElement("img");
+          imageContainer.src = image;
+          imageContainer.className = "iamge-gallery";
+          imageContainer.onclick = () => {
+            this.makeActive(index, image);
+          }
+          this.galleryContainer.appendChild(imageContainer);
 
-        image.className = "current-image";
+          return imageContainer;
+        });
 
-        this.galleryContainer.appendChild(image);
+        this.makeActive(0, images[0]);
+        
 
         this.backArrow.onclick = () => {
-            this.currentImage = this.currentImage - 1 === -1 ? images.length -1 : this.currentImage - 1;
-            image.src = images[this.currentImage];
+            this.currentIndexImage = this.currentIndexImage - 1 === -1 ? images.length -1 : this.currentIndexImage - 1;
+            makeActive(this.currentIndexImage, images[this.currentIndexImage]);
         }
 
         this.nextArrow.onclick = () => {
-            this.currentImage = this.currentImage + 1 === images.length ? 0 : this.currentImage + 1;
-            image.src = images[this.currentImage];
+            this.currentIndexImage = this.currentIndexImage + 1 === images.length ? 0 : this.currentIndexImage + 1;
+            makeActive(this.currentIndexImage, images[this.currentIndexImage]);
         }
+    }
+
+    makeActive(index, image) {
+        if (this.activeImageInGallety) {
+            this.activeImageInGallety.classList.remove("current-image-in-gallery");
+        }
+
+        const nextActiveImage = this.containerImages[index];
+
+        this.currentImageContainer.src = image;
+        this.activeImageInGallety = nextActiveImage;
+        this.activeImageInGallety.classList.add("current-image-in-gallery");
 
     }
 
